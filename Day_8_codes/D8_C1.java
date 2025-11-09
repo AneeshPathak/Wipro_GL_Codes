97c39cfa47ff29b03d97d2c841ce22ba82d1423f
Aneesh_Pathak <aneeshpathak888@gmail.com>
2025-11-09 17:49:45 +0530

    Day 8 problem statement and code added

---
+++ Day_8_codes/D8_C1.java
@@ -0,0 +1,43 @@
+/* Question 1 - You are required to build a calculator using concept of multithreading which performs the following arithematic operations:
+
+                Sum, difference, product , ratio, power.
+
+                Input Format  - two line separated integers
+
+    ****FILE NAME IS GIVEN DIFFERENTLY FOR SORTING OF FILES DAY WISE.***** */
+
+// Answer 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        A1 a = new A1(x, y);
+        
+        Thread t1 = new Thread(a);
+        t1.start();
+    }
+}
+
+// complete the class below and create the thread to achieve desired output
+class A1 implements Runnable {
+    int i, j;
+
+    A1(int x, int y) {
+        i = x;
+        j = y;
+    }
+
+    public void run() {
+        System.out.println("ARITHEMATIC OPERATIONS"); 
+        System.out.println("SUM " + (i + j));
+        System.out.println("DIFFERENCE " + (i - j));
+        System.out.println(" PRODUCT  " + (i * j));  
+        System.out.println("RATIO  " + (i / j));     
+        System.out.println("POWER  " + Math.pow(i, j)); 
+        System.out.println("END OF A");
+    }
+}
